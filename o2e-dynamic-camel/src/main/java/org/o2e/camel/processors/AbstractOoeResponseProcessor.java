c3875ecce898fba086a787b7b72766ebddc2fc46
Jeff Segal <jeffrey.segal@gmail.com>
2013-06-12 13:03:14 -0400

    Added support for extensible Camel Processors on Routes

---
+++ o2e-dynamic-camel/src/main/java/org/o2e/camel/processors/AbstractOoeResponseProcessor.java
@@ -0,0 +1,13 @@
+package org.o2e.camel.processors;
+
+import org.apache.camel.Processor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jeff
+ * Date: 6/11/13
+ * Time: 4:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class AbstractOoeResponseProcessor implements Processor {
+}
